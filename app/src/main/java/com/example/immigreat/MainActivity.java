f5dd2988b78d4d5a765857b9333d65bc657b6e96
neilkale <neil.kale@gmail.com>
2020-04-02 14:14:14 -0400

    Destroying Text Content XML and Java pages bc the Scrolling Activity is too weird to work with

--- app/src/main/java/com/example/immigreat/MainActivity.java
+++ app/src/main/java/com/example/immigreat/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.immigreat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void performStartMenu(View v) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 
     //Kim's update
