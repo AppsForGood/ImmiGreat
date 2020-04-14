186dea3f2d84a38d68a61b2adaf287ad4e788bc6
neilkale <neil.kale@gmail.com>
2020-04-13 20:01:16 -0400

    Added a laws menu activity and built it out. Added the logo designed by Sri to the main page Added a text page activity - this still needs to be built out.

--- app/src/main/java/com/example/immigreat/MainActivity.java
+++ app/src/main/java/com/example/immigreat/MainActivity.java
@@ -12,46 +12,52 @@ public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "MainActivity";
 
+    /**
+     *
+     **/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     *
+     */
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG,"onStart");
+        Log.i(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG,"onResume");
+        Log.i(TAG, "onResume");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG,"onStop");
+        Log.i(TAG, "onStop");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG,"onPause");
+        Log.i(TAG, "onPause");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(TAG,"onRestart");
+        Log.i(TAG, "onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG,"onDestroy");
+        Log.i(TAG, "onDestroy");
     }
 
     @Override
--- app/src/main/java/com/example/immigreat/MenuActivity.java
+++ app/src/main/java/com/example/immigreat/MenuActivity.java
@@ -2,7 +2,9 @@ package com.example.immigreat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void performStartLawsMenu(View v) {
+        Intent intent = new Intent(this, LawsMenuActivity.class);
+        startActivity(intent);
     }
 }
