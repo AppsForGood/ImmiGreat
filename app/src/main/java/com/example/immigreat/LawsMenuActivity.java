5dd66edd750064ed277d21c94255d104beb4adb2
neilkale <neil.kale@gmail.com>
2020-04-16 12:02:34 -0400

    The only fully built out page is: main --> laws --> education We still need to add content.
    This update is essentially the completion of draft 1 of the code. Now we just add content. This edition allows one string of content per page along with a heading and subheading.
    
    Future steps: Implement fragments to allow more diverse content. Move the content strings to SQLLite for efficient storage and retrieval.

--- app/src/main/java/com/example/immigreat/LawsMenuActivity.java
+++ app/src/main/java/com/example/immigreat/LawsMenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.immigreat;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LawsMenuActivity extends AppCompatActivity {
 
@@ -10,4 +13,29 @@ public class LawsMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laws_menu);
     }
+
+    /**
+     * This method is reusable in all situations where content must be written to the text page activity.
+     * It sends the content, heading, and subheading to the activity via intent.
+     * @param v
+     * @param content The main information which is to be displayed on the text page.
+     * @param heading The heading for the text page.
+     * @param subheading The subheading for the text page.
+     */
+    public void performStartPage(View v, String content, String heading, String subheading) {
+        Intent intent = new Intent(this, TextPageActivity.class);
+        intent.putExtra("CONTENT", content);
+        intent.putExtra("HEADING", heading);
+        intent.putExtra("SUBHEADING", subheading);
+
+        startActivity(intent);
+    }
+
+    public void performStartEducationPage(View v) {
+        String content = getResources().getString(R.string.educationPageContentStr);
+        String heading = getResources().getString(R.string.lawsHeadingStr);
+        String subheading = getResources().getString(R.string.lawsEducationHeadingStr);
+        performStartPage(v, content, heading, subheading);
+    }
+
 }
--- app/src/main/java/com/example/immigreat/TextPageActivity.java
+++ app/src/main/java/com/example/immigreat/TextPageActivity.java
@@ -2,13 +2,37 @@ package com.example.immigreat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TextPageActivity extends AppCompatActivity {
 
+    /**
+     * The onCreate is overridden to allow the text on the page to be set as the page is created. By default, the page is empty.
+     * The intent that creates it must send along string extras with the desired content, heading, and subheading.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_page);
+
+        Intent intent = getIntent();
+
+        TextView content = (TextView) findViewById(R.id.textPageContent);
+        content.setText(intent.getStringExtra("CONTENT"));
+
+        TextView heading  = (TextView) findViewById(R.id.textPageHeading);
+        heading.setText(intent.getStringExtra("HEADING"));
+
+        TextView subheading = (TextView) findViewById(R.id.textPageSubheading);
+        subheading.setText(intent.getStringExtra("SUBHEADING"));
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
 }
