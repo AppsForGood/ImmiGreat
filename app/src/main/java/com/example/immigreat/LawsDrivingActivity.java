50a119736dfc7574671ca24965574815846e20f6
srilasya <sporuri@wpi.edu>
2020-04-27 13:48:04 -0400

    Sri's update

--- app/src/main/java/com/example/immigreat/LawsDrivingActivity.java
+++ app/src/main/java/com/example/immigreat/LawsDrivingActivity.java
@@ -22,23 +22,28 @@ public class LawsDrivingActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    //This method opens up the Applying for a Class D License Page once the associated button is clicked
     public void performStartApplyingPage(View v) {
         String subHeading = getResources().getString(R.string.lawsDrivingApplyingLicenseHeadingStr);
         String heading = getResources().getString(R.string.lawsDrivingHeadingStr);
         performStartPage(v, heading, subHeading);
     }
 
+    //This method opens up the Foreign Policy Page once the associated button is clicked
     public void performStartForeignPolicyPage(View v) {
         String subHeading = getResources().getString(R.string.lawsDrivingForeignPolicyHeadingStr);
         String heading = getResources().getString(R.string.lawsDrivingHeadingStr);
         performStartPage(v, heading, subHeading);
     }
 
+    //This method opens up the JOL Requirements Page once the associated button is clicked
     public void performStartJOLPage(View v) {
         String subHeading = getResources().getString(R.string.lawsDrivingJOLHeadingStr);
         String heading = getResources().getString(R.string.lawsDrivingHeadingStr);
         performStartPage(v, heading, subHeading);
     }
+
+    //This method opens up the Cell Phone Laws Page once the associated button is clicked
     public void performStartCellPhonePage(View v) {
         String subHeading = getResources().getString(R.string.lawsDrivingCellPhoneHeadingStr);
         String heading = getResources().getString(R.string.lawsDrivingHeadingStr);
