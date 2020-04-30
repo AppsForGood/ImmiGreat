055187cdbe6517450bad6df308a5f96ca130935c
srilasya <sporuri@wpi.edu>
2020-04-30 14:38:58 -0400

    working link in applying page

--- app/src/main/java/com/example/immigreat/LawsKnowYourRightsFragment.java
+++ app/src/main/java/com/example/immigreat/LawsKnowYourRightsFragment.java
@@ -2,22 +2,19 @@ package com.example.immigreat;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LawsDrivingApplyingFragment#newInstance} factory method to
+ * Use the {@link LawsKnowYourRightsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LawsDrivingApplyingFragment extends Fragment {
+public class LawsKnowYourRightsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -27,7 +24,7 @@ public class LawsDrivingApplyingFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LawsDrivingApplyingFragment() {
+    public LawsKnowYourRightsFragment() {
         // Required empty public constructor
     }
 
@@ -37,11 +34,11 @@ public class LawsDrivingApplyingFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LawsApplying.
+     * @return A new instance of fragment LawsKnowYourRights.
      */
     // TODO: Rename and change types and number of parameters
-    public static LawsDrivingApplyingFragment newInstance(String param1, String param2) {
-        LawsDrivingApplyingFragment fragment = new LawsDrivingApplyingFragment();
+    public static LawsKnowYourRightsFragment newInstance(String param1, String param2) {
+        LawsKnowYourRightsFragment fragment = new LawsKnowYourRightsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,12 +59,6 @@ public class LawsDrivingApplyingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_laws_applying, container, false);
-    }
-
-    //Method used to allows users to click link in the text
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        TextView link1 = (TextView) getView().findViewById(R.id.applyingContent);
-        link1.setMovementMethod(LinkMovementMethod.getInstance());
+        return inflater.inflate(R.layout.fragment_laws_know_your_rights, container, false);
     }
 }
